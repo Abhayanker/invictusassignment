e2e1923281fd57d6185366b4760d6a72cce9ca5c
Abhayanker <ankurabhay120@gmail.com>
2021-05-16 23:15:18 +0530

    commit 1st

---
+++ src/index.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const findMostFrequent = (str = '', num = 1) => {
+  const strArr = str.split(' ');
+  const map = {};
+  strArr.forEach(word => {
+     if(map.hasOwnProperty(word)){
+        map[word]++;
+     }else{
+        map[word] = 1;
+     }
+  });
+  const frequencyArr = Object.keys(map).map(key => [key, map[key]]);
+  frequencyArr.sort((a, b) => b[1] - a[1]);
+  return frequencyArr.slice(0,num);
+  
+};
+
+
+function handleClick() {
+  fetch('https://raw.githubusercontent.com/invictustech/test/main/README.md')
+  .then(response => response.text())
+  .then(data => {
+      console.log(data);
+      const num=document.getElementById("myInput").value;
+      const array=findMostFrequent(data, num);
+      const renderwords=(c,d)=>{
+          return(
+            <tr key={d}>
+            <td>{c[0]}</td>
+            <td>{c[1]}</td>
+            </tr>
+           )
+        };
+      ReactDOM.render(<div>
+        <table border="1" >
+        <thead><tr>
+          <th>Words</th>
+          <th>count</th>
+          </tr></thead>
+          <tbody>
+            {array.map(renderwords)}
+          </tbody>
+        </table>
+      </div>
+      
+      ,document.getElementById('root'));
+  });
+  
+
+}
+
+
+ReactDOM.render(
+  
+  
+<div>
+      <input type="text" placeholder="Type something..." id="myInput"></input>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <button onClick={handleClick} id="btn">click me</button>
+     
+      
+</div>,
+  document.getElementById('root')
+);
